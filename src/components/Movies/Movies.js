5c4597c5b1ee9f0d49d1addddfb09dc53f43a4d7
SofiyaYehorova <yehorova66@gmail.com>
2023-02-21 14:55:59 +0200

    fix

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -11,11 +11,15 @@ import css from './Movies.module.css'
 
 const Movies = () => {
     const [movies, setMovies] = useState([]);
+
     const {loading, page, filterParams} = useSelector(state => state.movies)
+    const {genre} = useSelector(state => state.genres);
+
     const dispatch = useDispatch();
 
     const [query, setQuery] = useSearchParams();
     const queryPage = query.get('page');
+
 
     useEffect(() => {
         moviesAction.setPage(queryPage)
@@ -23,19 +27,36 @@ const Movies = () => {
             .then(({payload}) => setMovies(payload.results))
     }, [page, queryPage]);
 
+    useEffect(() => {
+        if (genre) {
+            setMovies(movies.filter(movie => movie.genre_ids.includes(genre.id)))
+        } else {
+            dispatch(moviesAction.getAllMovies({page: queryPage}))
+                .then(({payload}) => setMovies(payload.results))
+        }
+    }, [genre])
+
+    useEffect(() => {
+        setMovies(movies.filter(movie => movie.title.includes(filterParams)))
+        if (filterParams === '') {
+            dispatch(moviesAction.getAllMovies({page: queryPage}))
+                .then(({payload}) => setMovies(payload.results))
+        }
+    }, [filterParams])
+
     return (
         <div>
             {loading && <h1>Loading</h1>}
             <div className={css.Movies}>
                 {movies.map(movie => <Movie key={movie.id} movie={movie}/>)}
             </div>
-            {/*{filterParams !== '' &&*/}
-            {/*    <div>*/}
-            {/*        <button onClick={() => dispatch(moviesAction.setFilterParams(''))}>*/}
-            {/*            Click me!*/}
-            {/*        </button>*/}
-            {/*    </div>*/}
-            {/*}*/}
+            {filterParams !== '' &&
+                <div>
+                    <button onClick={() => dispatch(moviesAction.setFilterParams(''))}>
+                        Click me!
+                    </button>
+                </div>
+            }
             <Pagination queryPage={queryPage}/>
         </div>
 
