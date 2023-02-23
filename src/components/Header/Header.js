917f7425a29a69cd91b0f913625151b1e919c85e
SofiyaYehorova <yehorova66@gmail.com>
2023-02-23 18:32:51 +0200

    fuck

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {SearchMovie} from "../SearchMovie/SearchMovie";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-
+import icon from '../../image/user-icon.png';
 import '../../index.css'
 import {faLightbulb} from "@fortawesome/free-solid-svg-icons";
 
@@ -13,7 +13,8 @@ const Header = () => {
             <div className={'wrapper'}>
                 hello header
                 <div className={'icon-user'}>
-                    <img scr="https://w7.pngwing.com/pngs/627/693/png-transparent-computer-icons-user-user-icon-thumbnail.png" alt='user-icon'/>
+                    <img src={icon}/>
+                    {/*<img scr="https://w7.pngwing.com/pngs/627/693/png-transparent-computer-icons-user-user-icon-thumbnail.png" alt='user-icon'/>*/}
                     {/*<FontAwesomeIcon  icon="fa-solid fa-user" />*/}
                     {/*<FontAwesomeIcon className={'icon'} icon={"fa-solid fa-user"}></FontAwesomeIcon>*/}
                     {/*<FontAwesomeIcon className={'icon'} icon={faLightbulb}></FontAwesomeIcon>*/}
